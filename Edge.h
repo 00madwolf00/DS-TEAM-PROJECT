de0f0a749c3c5e38f33a96dfe98b211759b00e26
Jasmine Heymann <121975438+verynotjas@users.noreply.github.com>
2025-04-19 21:11:08 -0400

    Updated Edge.h based on project context and past class example

--- Edge.h
+++ Edge.h
@@ -1,16 +1,29 @@
+/*
+* Edge.h
+* Names: Allicyan Chin, Jaheem Clayton, Jasmine Heymann
+* Course: COP-3415
+* Last Modified: 04/19/2025
+*/
+
 #ifndef EDGE_H
 #define EDGE_H
 
-#include "AirportNode.h"
-using namespace std;
-
 class Edge {
 public:
-    AirportNode* to;
-    double distance;
-    double cost;
+    Edge(int src = 0, int dest = 0, int distance = 0, int cost = 0) : 
+        src(src), dest(dest), distance(distance), cost(cost) {}
 
-    Edge(AirportNode* dest, double dist, double cost);
+    bool operator<(const Edge& other) const {
+        if(this->distance < other.distance) {
+            return true;
+        }
+        return false;
+    }
+    
+    int src;
+    int dest;
+    int distance;
+    int cost;
 };
 
-#endif //EDGE_H
+#endif
