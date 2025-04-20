d7e406a3e10df050678f7e0bf57696311eb38eac
Jasmine Heymann <121975438+verynotjas@users.noreply.github.com>
2025-04-20 10:56:51 -0400

    Update AirportNode.cpp

--- AirportNode.cpp
+++ AirportNode.cpp
@@ -1,5 +1,17 @@
 #include "AirportNode.h"
+#include "Edge.h"
 
-AirportNode::AirportNode(string n){
-    name = n;
+AirportNode::AirportNode(string& code, string& city, string& state) : code(code), city(city), state(state), visited(false) {}
+
+AirportNode::~AirportNode(){
+    for(Edge* edge : outgoingEdges){
+        delete edge;
+    }
+
+    outgoingEdges.clear();
+}
+
+void AirportNode::addEdge(AirportNode* destination, double distance, double cost){
+    Edge* newEdge = new Edge(destination, distance, cost);
+    outgoingEdges.push_back(newEdge);
 }
